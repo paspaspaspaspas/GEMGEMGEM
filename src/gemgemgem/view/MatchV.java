13338d0fef456ca7e5646a13a49954ab6e250645
SamuelePasini_732232 <s.pasini014@studenti.unibs.it>
2022-01-22 12:18:18 +0100

    Progressi

--- src/gemgemgem/view/MatchV.java
+++ src/gemgemgem/view/MatchV.java
@@ -13,15 +13,16 @@ import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
 
-public class MatchV implements MouseMotionListener{
+public class MatchV implements MouseMotionListener, MouseListener {
 
 	private JFrame frame;
 	private JPanel panel;
-	
+
 	private BoardV board;
 	private PlayerV player1;
 	private PlayerV player2;
@@ -48,6 +49,7 @@ public class MatchV implements MouseMotionListener{
 	 */
 	public MatchV() {
 		initialize();
+
 	}
 
 	/**
@@ -55,43 +57,83 @@ public class MatchV implements MouseMotionListener{
 	 */
 	private void initialize() {
 		frame = new JFrame();
+
 		frame.setBounds(50, 50, 1024, 768);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		selectedCard = new SelectedCardV();
 		selectedCard.setBounds(100, 100, 100, 100);
 		frame.getContentPane().add(selectedCard);
-		
+
 		panel = new JPanel();
 		panel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
 		panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
 		frame.getContentPane().add(panel);
 
 		player1 = new PlayerV();
-		player1.setBounds(0, 0, (int)(frame.getWidth() * 0.2), frame.getHeight());
+		player1.setBounds(0, 0, (int) (frame.getWidth() * 0.2), frame.getHeight());
 		panel.add(player1);
-		
+
 		board = new BoardV();
-		board.setBounds((int)(frame.getWidth() * 0.2), 0, (int)(frame.getWidth() * 0.6), frame.getHeight());
+		board.setBounds((int) (frame.getWidth() * 0.2), 0, (int) (frame.getWidth() * 0.6), frame.getHeight());
 		panel.add(board);
-		
-        player2 = new PlayerV();
-        player2.setBounds((int)(frame.getWidth() * 0.8), 0, (int)(frame.getWidth() * 0.2), frame.getHeight());
-        panel.add(player2);
-        
-        frame.getContentPane().addMouseMotionListener(this);
+
+		player2 = new PlayerV();
+		player2.setBounds((int) (frame.getWidth() * 0.8), 0, (int) (frame.getWidth() * 0.2), frame.getHeight());
+		panel.add(player2);
+
+		frame.getContentPane().addMouseMotionListener(this);
+		frame.getContentPane().addMouseListener(this);
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		selectedCard.setBounds((int)(e.getPoint().getX()-50), (int)(e.getPoint().getY()-50), 100, 100);
-		selectedCard.repaint();
+
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
+		//TODO QUANDO VA SU UN COMPONENT CLICKABLE PERDO LA POSIZIONE DEL MOUSE.
 		
+		if (selectedCard.isSelected()) {
+			selectedCard.setBounds((int) (e.getPoint().getX() - 50), (int) (e.getPoint().getY() - 50), 100, 100);
+		}else {
+			selectedCard.setBounds((int) (e.getPoint().getX() - 10), (int) (e.getPoint().getY() - 10), 20, 20);
+		}
+		selectedCard.repaint();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		selectedCard.setSelected(!selectedCard.isSelected());
+		mouseMoved(e);
 		
+		//TODO NON MI SEMBRA CHE SENTA SEMPRE IL CLICK
+		System.out.println("I hear you mate");
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- src/gemgemgem/view/SelectedCardV.java
+++ src/gemgemgem/view/SelectedCardV.java
@@ -7,21 +7,34 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Point2D;
 
-public class SelectedCardV extends CardV{
-	
+public class SelectedCardV extends CardV {
+
+	private boolean isSelected;
+
 	public SelectedCardV() {
 		super(Color.PINK, Color.MAGENTA, false);
+		this.isSelected = false;
 	}
-	
+
 	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g2.setStroke(new BasicStroke(4f));
+		if (isSelected) {
+			super.paintComponent(g);
+		} else {
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			g2.setStroke(new BasicStroke(4f));
+			g2.setColor(Color.GREEN);
+			g2.fillOval(0, 0, 20, 20);
+		}
 
-		g2.setColor(getMainColor());
-		g2.fillRect(0, 0, 100, 100);
 	}
 
-	
+	public boolean isSelected() {
+		return isSelected;
+	}
+
+	public void setSelected(boolean isSelected) {
+		this.isSelected = isSelected;
+	}
+
 }
